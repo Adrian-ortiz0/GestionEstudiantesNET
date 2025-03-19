5267b1d91f3b7817bbfcc3be03fc5e239677bf12
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-03-19 11:56:41 -0500

    feat✨:  currently working on http method, then i will use repository patterns to activate functionalities

--- GestionEstudiantes/Pages/Estudiantes.cshtml.cs
+++ GestionEstudiantes/Pages/Estudiantes.cshtml.cs
@@ -1,12 +1,30 @@
+using EstudiantesCore.DTOs;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace GestionEstudiantes.Pages
 {
+    [IgnoreAntiforgeryToken]
     public class EstudiantesModel : PageModel
     {
         public void OnGet()
         {
+        }
+
+        [HttpPost]
+        public IActionResult OnPostCrearEstudiante(EstudianteDto estudianteDto)
+        {
+
+            try
+            {
+                return StatusCode(200, "Estudiante creado");
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
     }
 }
---
+++ GestionEstudiantes/wwwroot/js/Estudiantes.js
@@ -0,0 +1,39 @@
+﻿// Configuracion por defecto
+$.ajaxSetup({
+    data: {
+        __RequestVerificationToken: document.getElementsByName(
+            "__RequestVerificationToken"
+        )[0].value
+    }
+});
+
+//Guarda y valida estudiantes
+async function Salvar()
+{
+    const form = $("#FormEstudiante").dxForm("instance");
+    const valido = form.validate().isValid;
+
+    if (valido)
+    {
+        alert("Formulario valido");
+        const data = form.option("formData");
+        console.log(data);
+        await EnviarAlServidor(data);
+    }
+}
+
+async function EnviarAlServidor(dataFormulario) {
+
+    try {
+
+        const response = await $.ajax({
+            method: "POST",
+            url: "/Estudiantes?handler=CrearEstudiante",
+            data: dataFormulario
+        });
+        alert("Usuario agregado");
+        const form = $("#FormEstudiante").dxForm("instance").resetValues();
+    } catch (error) {
+        console.error(error, "Algo salio mal")
+    }
+}
